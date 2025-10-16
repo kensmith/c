b3fa7e026972d770523b5e068ad3dd11200bd395
Ken Smith <fwxgq.oysqv@fastmail.com>
2025-10-16 13:56:50 -0700

    simplified help command

--- cascade.go
+++ cascade.go
@@ -1,15 +1,6 @@
 package main
 
-/*
-func tryOpCascade(line string, stack *Stack, ops OpMap) error {
-	err := tryOp(line, stack, ops)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-*/
+import "fmt"
 
 func cascade(line string, stack *Stack, ops *Ops) error {
 	err := tryExpr(line, stack)
@@ -22,31 +13,7 @@ func cascade(line string, stack *Stack, ops *Ops) error {
 		return nil
 	}
 
-	err = tryCommands(line, stack, ops)
-	if err != nil {
-		return err
-	}
+	fmt.Println(ops.Help())
 
 	return nil
 }
-
-/*
-func tryCascade(line string, stack *Stack, operators OperatorMap) error {
-	err := tryExpr(line, stack)
-	if err == nil {
-		return nil
-	}
-
-	err = tryOperator(line, stack, operators)
-	if err == nil {
-		return nil
-	}
-
-	err = tryCommands(line, stack, operators)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-*/
